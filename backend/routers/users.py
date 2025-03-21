b2dcb2942f8574a91f54b34dc8256a338ec9f3b8
Portavion <156545430+Portavion@users.noreply.github.com>
2025-03-21 15:29:55 +0000

    feat: backend init setup

---
+++ backend/routers/users.py
@@ -0,0 +1,10 @@
+from fastapi import APIRouter
+
+router = APIRouter(prefix="/users",
+                   tags=["users"],
+                   responses={404: {"description": "Not found"}}, )
+
+
+@router.get("/")
+async def read_users():
+    return [{"username", "Guillaume"}, {"username", "Porta"}]
